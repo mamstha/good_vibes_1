ba2750ce71fc5edf15132027d16685f0ab716b3c
mamstha <86461385+mamstha@users.noreply.github.com>
2022-01-27 18:59:22 +0545

    GoodVibes

--- pages/hero-banner.php
+++ pages/hero-banner.php
@@ -1,16 +1,27 @@
-<div class="container hero-banner">
+<div class="hero-banner">
+    <div class="container">
     <div class="row">
         <div class="hero-banner-text col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
 
-        <h1>Restless To Restful Plus Mind</h1>
-        <p>Good Vibes - Binaural Beats provides an old practice meditation therapy into a form of music. You can choose various Binaural Beats Therapy for meditation, sleep, study</p>
+        <div class="first-content">
+            <h1>Restless To Restful Plus Mind</h1>
+            <p>Good Vibes - Binaural Beats provides an old practice meditation therapy into a form of music. You can choose various Binaural Beats Therapy for meditation, sleep, study</p>
 
-        <div class="btn-group">
+        
+            <div class="btn-group">
             <button type="button" class="btn btn-primary">Get Started</button>
 
             <div class="arrow">
                 <i class="far fa-arrow-right"></i>
             </div>
+        </div>
+
+        <div class="img1">
+            <img src="img/utilities/scribble.svg" alt="">
+        </div>
+
+        <div class="img2">
+            <img src="img/utilities/dots.svg" alt="">
         </div>
 
         <div class="social-icon">
@@ -29,9 +40,15 @@
             </div>     
         </div>
         </div>
+        </div>
+
+        
 
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 hero-banner-img">
             <img src="img\utilities\casual-life-3d-meditation-1.png" alt="" >
+
+            
         </div>
+    </div>
     </div>
 </div>
